af6b8f66f6657e039c8405fbe5255d6e324bbee4
sherry880929 <52311790+sherry880929@users.noreply.github.com>
2020-01-16 23:52:20 +0800

    Create [C_AR53-易] 學生成績排序.cpp

---
+++ [C_AR53-易] 學生成績排序.cpp
@@ -0,0 +1,20 @@
+#include <iostream>  
+#include <algorithm>//sort()  
+using namespace std;  
+  
+int main() {  
+    // [C_AR53-易] 學生成績排序  
+    int n;//學生數  
+    cin >> n;  
+    int grade[n];//成績  
+    for(int i = 0;i < n;i++)  
+    {  
+        cin >> grade[i];  
+    }  
+    sort(grade,grade+n);  
+    for(int i = 0;i < n;i++)  
+    {  
+        cout << grade[i] << endl;;  
+    }  
+    return 0;  
+}  
